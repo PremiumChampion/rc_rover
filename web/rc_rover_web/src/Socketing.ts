85ef1bdeb5834928513ba6e8a91e08ffcf8703f2
Woyte <t.woityschyn.github@outlook.de>
2023-04-16 20:20:08 +0200

    cleanup

--- web/rc_rover_web/src/Socketing.ts
+++ web/rc_rover_web/src/Socketing.ts
@@ -1,17 +1,6 @@
 import { io } from "socket.io-client";
-import { AsyncSetup } from "./AsyncSetup";
-
 
 const socket = io();
-
-// socket_setup.registerSetup(new Promise((res) => {
-//     socket.connect();
-//     socket.on("connect", () => {
-//         res(null);
-//     });
-// }));
-// socket_setup.executeAsyncSetup();
-
 
 
 export { socket };
